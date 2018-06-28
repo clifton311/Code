9991dc59fb2066005b91ea502c4a7b8365a6325d
clifton311 <40072774+clifton311@users.noreply.github.com>
2018-06-27 22:04:29 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 const numArray = [2,3,4,4,5,5,5,6];
 
-// const median = (array) => {
-// let mid = Math.floor(array.length/2);
-// let output = 0
-// let sorted = array.sort(function(a,b){return a-b;})
-//     if (array[mid] % 2 === 0){
-//       output = array[mid];
-//     } else if (array[mid] % 2 === 1){
-//       output = Math.round((array[mid-1] + array[mid])/2);
-//     }
-//   return output;
-// }
-// console.log('The Median is ' + median(numArray))
+const median = (array) => {
+let mid = Math.floor(array.length/2);
+let output = 0
+let sorted = array.sort(function(a,b){return a-b;})
+    if (array[mid] % 2 === 0){
+      output = array[mid];
+    } else if (array[mid] % 2 === 1){
+      output = Math.round((array[mid-1] + array[mid])/2);
+    }
+  return output;
+}
+console.log('The Median is ' + median(numArray))
 
-// const mean = numArray.reduce((total, curr) => {
-//  total += curr
-//  return total;
-// },0)
+const mean = numArray.reduce((total, curr) => {
+ total += curr
+ return total;
+},0)
 
-// let output = Math.floor(mean/numArray.length);
-// console.log('The Mean is '+ output)
+let output = Math.floor(mean/numArray.length);
+console.log('The Mean is '+ output)
